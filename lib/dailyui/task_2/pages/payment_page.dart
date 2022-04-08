67ddc239ffa7e47e6e217e32b87365c835ac1a31
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2022-04-07 22:37:16 -0500

    start on second ui design

---
+++ lib/dailyui/task_2/pages/payment_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PaymentPage extends StatelessWidget {
+  const PaymentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0x7968FAFF),
+      body: Column(
+        children: [
+          Text('Input '),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'dailyui/pages/login_page.dart';
+import 'dailyui/task_1/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
